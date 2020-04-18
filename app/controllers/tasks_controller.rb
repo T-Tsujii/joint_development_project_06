6eb7221e79bff710c7e13c452a9a58c30cf98e68
徳久　貴裕 <tokuhisatakahiro@tokuhisatakahironoMacBook-Pro.local>
2020-04-18 11:56:21 +0900

    3回目

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,14 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    Task.create(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
 
